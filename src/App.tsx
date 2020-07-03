adfb0530ce5037346394afe4bed703bd2e442be7
Nicholas Broström <nicholas.brostrom@effectsoft.se>
2020-07-03 23:39:29 +0200

    Test commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,26 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./tenor.gif";
+import "./App.css";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<div className="App">
+			<header className="App-header">
+				<img src={logo} className="App-logo" alt="logo" />
+				<p>
+					Edit <code>src/App.tsx</code> and save to reload.
+				</p>
+				<button
+					className="App-link"
+					onClick={async () => {
+						await fetch("http://165.22.118.168:8080/random/621035571057524737");
+					}}
+				>
+					Random MarketPlace
+				</button>
+			</header>
+		</div>
+	);
 }
 
 export default App;
