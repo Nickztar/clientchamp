2d5d1769b96f050149d07618caa208e6223b31bc
Nicholas Broström <nicholas.brostrom@effectsoft.se>
2020-08-07 13:46:04 +0200

    Dev branch 🎹

--- src/App.tsx
+++ src/App.tsx
@@ -29,14 +29,14 @@ export default function App() {
                 {/* A <Switch> looks through its children <Route>s and
             renders the first one that matches the current URL. */}
                 <Switch>
+                    <Route path="/">
+                        <Home />
+                    </Route>
                     <Route path="/about">
                         <h2>About</h2>
                     </Route>
                     <Route path="/login">
                         <h2>Login</h2>
-                    </Route>
-                    <Route path="/">
-                        <Home />
                     </Route>
                 </Switch>
             </div>
